90275a8db2c81210ffbf3e8ff2790c8cdb4f21e9
mR4smussen <m.rasmussen.uni@gmail.com>
2022-07-26 23:28:27 +0200

    throw error if the folder already exist

--- commands/dl.js
+++ commands/dl.js
@@ -2,6 +2,7 @@ import { exec } from 'child_process';
 import chalk from 'chalk';
 import { join } from 'path'
 import * as url from 'url';
+import fs from 'fs';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -11,26 +12,30 @@ export default (packageName, packageVersions) => {
     const packagesAdded = [];
     let packagesLeft = packageVersions.length
     const scriptPath = join(__dirname, 'scripts', 'dl')
-    for (const v of packageVersions) {
-        exec(`${scriptPath} ${packageName} ${v}`, function(err, stdout){
-            if (err) {
-                console.log(err)
-            } else {
-                console.log(stdout)
-                packagesAdded.push(`${packageName}@${v}`)
-            }
+    for (let v of packageVersions) {
+        if (fs.existsSync(`${packageName}@${v}`)) {
+            console.log(chalk.red(`The folder ${packageName}@${v} already exists`))
             packagesLeft--;
-            // print result
-            if (packagesLeft <= 0) {
-                if (packagesAdded.length > 0) {
-                    for (const p of packagesAdded) {
-                        console.log(chalk.green(chalk.bold(`${p} was added to the new folder 
-                        ${process.cwd()}/${p}/auxProject`)))
-                    }
-                    console.log("\n")
+        }
+        else {
+            exec(`${scriptPath} ${packageName} ${v}`, function(err, stdout){
+                if (err) {
+                    console.log(err)
+                } else {
+                    packagesAdded.push(`${packageName}@${v}`)
                 }
-            }
-            
-        });
+                packagesLeft--;
+                if (packagesLeft <= 0) {
+                    if (packagesAdded.length > 0) {
+                        for (const p of packagesAdded) {
+                            console.log(chalk.green(chalk.bold(`${p} was added to the new folder 
+                            ${process.cwd()}/${p}/auxProject`)))
+                        }
+                        console.log("\n")
+                    }
+                }
+                
+            });
+        }
     }
   };
