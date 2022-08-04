532a283ca08e964b3493f65e342219fca69f3240
mR4smussen <m.rasmussen.uni@gmail.com>
2022-08-04 11:20:47 +0200

    Changed the new folder structure and fixed cases where "@" and "/" was a part of the package name

--- commands/dl.js
+++ commands/dl.js
@@ -9,38 +9,40 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 export default (packageName, packageVersions, options) => {
     const packagesAdded = [];
     let classDefs = "" // Used when the -c/--class flag is set
+    const nameFolder = packageName.replace('/', '_').replace('@', '+') // @ -> +, / -> _, not sure what to use as the new symbols...
     
     console.log("Estimated time: " + (2*packageVersions.length + " seconds"))
     // TODO: Give the user the option to choose a custom folder name
     // Make sure the folder does not already exist
-    if (fs.existsSync(packageName)) {
-        console.log(chalk.red(`The folder ${packageName} already exists`))
+    if (fs.existsSync(nameFolder)) {
+        console.log(chalk.red(`The folder ${nameFolder} already exists`))
         return;
     }
     for (let v of packageVersions) {
-        const folderName = (v ? `${packageName}@${v}` : `${packageName}`)
-        exec(`mkdir ${packageName}`);
-        exec(`cd ${packageName} && ${join(__dirname, 'scripts', 'installPackage')} ${folderName}`, function(err, stdout){
+        const pNameAndVersion = `${packageName}@${v}`
+        const versionFolder = `${v}.x`
+        exec(`mkdir ${nameFolder}`);
+        exec(`cd ${nameFolder} && ${join(__dirname, 'scripts', 'installPackage')} ${pNameAndVersion} ${versionFolder}`, function(err){
             if (err) {
                 console.log(err)
                 // If the folder was created before the error, remove the folder again
-                if(fs.existsSync(`${packageName}/${folderName}`)) {
-                    exec(`${join(__dirname, 'scripts', 'removeFolder')} ${packageName}/${folderName}`);
+                if(fs.existsSync(`${nameFolder}/${versionFolder}`)) {
+                    exec(`${join(__dirname, 'scripts', 'removeFolder')} ${nameFolder}/${versionFolder}`);
                 };
-                console.log(chalk.red(`The error was most likely caused by ${folderName} not being a valid package on npm.`))
+                console.log(chalk.red(`The error was most likely caused by ${pNameAndVersion} not being a valid package on npm.`))
             } else {
-                packagesAdded.push(`${folderName}`)
+                packagesAdded.push(`${pNameAndVersion}`)
 
                 // If the -c/--class flag is used, we output the class defs. of the versions in a "classDefs" text file in the new shared folder.
                 if (options.class) {
-                    const nodeRepl = spawn('node', { cwd: `${packageName}/${folderName}/auxProject` });
+                    const nodeRepl = spawn('node', { cwd: `${nameFolder}/${versionFolder}` });
                     nodeRepl.stdout.pipe(process.stdout);
 
                     nodeRepl.stdout.on('data', (data) => { classDefs += data; })
                     nodeRepl.stderr.on('data', (data) => { console.log(`stderr: ${data}`); })
 
                     nodeRepl.stdin.write(`const lib = require('${packageName}')\r\n`);
-                    nodeRepl.stdin.write(`console.log("${folderName} class:")\r\n`);
+                    nodeRepl.stdin.write(`console.log("${pNameAndVersion} class:")\r\n`);
                     nodeRepl.stdin.end('console.log(lib)\r\n');
 
                 }
@@ -54,23 +56,23 @@ export default (packageName, packageVersions, options) => {
         if (packagesAdded.length > 0) {
             for (const p of packagesAdded) {
                 console.log(chalk.green(chalk.bold(`${p} was added to the new folder
-                ${process.cwd()}/${packageName}/${p}/auxProject`)))
+                ${process.cwd()}/${nameFolder}/_version`)))
             }
             console.log("\n")
         }
 
         // If no version of the package was successfully downloaded, remove the folder.
-        fs.readdir(packageName, (err, files) => {
+        fs.readdir(nameFolder, (err, files) => {
             if (err) {
                 console.log("Err: " + err)
             } else if (!files.length) {
-                exec(`${join(__dirname, 'scripts', 'removeFolder')} ${packageName}`);
+                exec(`${join(__dirname, 'scripts', 'removeFolder')} ${nameFolder}`);
             } else {
                 // Not empty = a package have been successfully downloaded
 
                 // -c/--class flag
                 if (options.class) {
-                    fs.writeFile(`${packageName}/classDefs`, classDefs, err => {
+                    fs.writeFile(`${nameFolder}/classDefs`, classDefs, err => {
                         if (err) {
                         console.error(err);
                         }
--- commands/repl.js
+++ commands/repl.js
@@ -8,24 +8,27 @@ import fs from 'fs';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export default (packageName, packageVersion) => {
-    const folderName = (packageVersion ? `${packageName}@${packageVersion}` : `${packageName}`)
-    if (fs.existsSync(folderName)) {
-        console.log(chalk.red(`The folder ${folderName} already exists`))
+    const nameFolder = packageName.replace('/', '_').replace('@', '+') // @ -> +, / -> _, not sure what to use as the new symbols...
+    const versionFolder = (packageVersion ? `${packageVersion}.x` : `latestVersion`)
+    const pNameAndVersion = (packageVersion ? `${packageName}@${packageVersion}` : `${packageName}`)
+    if (fs.existsSync(nameFolder)) {
+        console.log(chalk.red(`The folder ${nameFolder} already exists`))
     } else {
-        exec(`${join(__dirname, 'scripts', 'installPackage')} ${folderName}`, function(err, stdout){
+        exec(`mkdir ${nameFolder}`);
+        exec(`cd ${nameFolder} && ${join(__dirname, 'scripts', 'installPackage')} ${pNameAndVersion} ${versionFolder}`, function(err){
             if (err) {
                 console.log(err)
                 // If the folder was created before the error, remove the folder again
-                if(fs.existsSync(`${folderName}`)) {
-                    exec(`${join(__dirname, 'scripts', 'removeFolder')} ${folderName}`);
+                if(fs.existsSync(`${nameFolder}`)) {
+                    exec(`${join(__dirname, 'scripts', 'removeFolder')} ${nameFolder}`);
                 }
-                console.log(chalk.red(`The error was most likely caused by ${folderName} not being a valid package on npm.`))
+                console.log(chalk.red(`The error was most likely caused by ${pNameAndVersion} not being a valid package on npm.`))
             }
             else {
-                console.log(chalk.green(chalk.bold(`\n${folderName} was successfully installed in the new folder 
-                ${process.cwd()}/${folderName}/auxProject`))); 
+                console.log(chalk.green(chalk.bold(`\n${pNameAndVersion} was successfully installed in the new folder
+                ${process.cwd()}/${nameFolder}/${versionFolder}`))); 
                 
-                process.chdir(`./${folderName}/auxProject`);
+                process.chdir(`./${nameFolder}/${versionFolder}`);
                 repl.start().write(`const lib = require('${process.cwd()}/node_modules/${packageName}')\r\n`)
             } 
         });
