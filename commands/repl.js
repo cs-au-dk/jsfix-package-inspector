a9c759c0643ab100c95518348b2a04f125ec7791
mR4smussen <m.rasmussen.uni@gmail.com>
2022-08-08 13:39:32 +0200

    changed dir for repl command to be in the /tmp folder

--- commands/repl.js
+++ commands/repl.js
@@ -1,9 +1,10 @@
-import { exec } from 'child_process';
+import { exec, execSync } from 'child_process';
 import repl from 'repl'
 import chalk from 'chalk';
 import { join } from 'path'
 import * as url from 'url';
 import fs from 'fs';
+import tmp from 'tmp';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -14,23 +15,26 @@ export default (packageName, packageVersion) => {
     if (fs.existsSync(nameFolder)) {
         console.log(chalk.red(`The folder ${nameFolder} already exists`))
     } else {
-        exec(`mkdir ${nameFolder}`);
-        exec(`cd ${nameFolder} && ${join(__dirname, 'scripts', 'installPackage')} ${pNameAndVersion} ${versionFolder}`, function(err){
-            if (err) {
-                console.log(err)
-                // If the folder was created before the error, remove the folder again
-                if(fs.existsSync(`${nameFolder}`)) {
-                    exec(`${join(__dirname, 'scripts', 'removeFolder')} ${nameFolder}`);
+        tmp.setGracefulCleanup();
+        tmp.dir({unsafeCleanup: true}, function _dirSync(err, path) {
+            if (err) throw err;
+
+            process.chdir(`${path}`)
+            execSync(`mkdir ${nameFolder}`);
+
+            exec(`cd ${nameFolder} && ${join(__dirname, 'scripts', 'installPackage')} ${pNameAndVersion} ${versionFolder}`, function(err){
+                if (err) {
+                    console.log(err)
+                    console.log(chalk.red(`The error was most likely caused by ${pNameAndVersion} not being a valid package on npm.`))
                 }
-                console.log(chalk.red(`The error was most likely caused by ${pNameAndVersion} not being a valid package on npm.`))
-            }
-            else {
-                console.log(chalk.green(chalk.bold(`\n${pNameAndVersion} was successfully installed in the new folder
-                ${process.cwd()}/${nameFolder}/${versionFolder}`))); 
-                
-                process.chdir(`./${nameFolder}/${versionFolder}`);
-                repl.start().write(`const lib = require('${process.cwd()}/node_modules/${packageName}')\r\n`)
-            } 
+                else {
+                    console.log(chalk.green(chalk.bold(`\n${pNameAndVersion} was successfully installed in the new folder
+                    ${process.cwd()}/${nameFolder}/${versionFolder}`))); 
+                    
+                    process.chdir(`./${nameFolder}/${versionFolder}`);
+                    repl.start().write(`const lib = require('${process.cwd()}/node_modules/${packageName}')\r\n`)
+                } 
+            });
         });
     }
   };
